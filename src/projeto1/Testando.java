90cc6e018865571abb06f74eac085613d87fa5af
Lívia Oliveira <livia.oliveira.almeida@hotmail.com>
2021-08-12 12:42:58 -0300

    subindo projeto

---
+++ src/projeto1/Testando.java
@@ -0,0 +1,11 @@
+package projeto1;
+
+public class Testando {
+
+	public static void main(String[] args) {
+		teste_vocacional_metodo inteligencia = new teste_vocacional_metodo();
+		
+		inteligencia.inteligencias();
+	}
+
+}
